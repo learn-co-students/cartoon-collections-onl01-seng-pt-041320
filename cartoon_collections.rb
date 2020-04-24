3447154fc2d2b0d673b7b7d557cad6f275ef391d
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-24 04:32:57 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,19 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+def roll_call_dwarves(array) # code an argument here
+  array.each_with_index do|name, index| #each_with_index is similar to a while-counter combination
+    puts "#{index + 1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.each_with_index.map {|word| word.capitalize << "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? {|i| i.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)# code an argument here
+cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find {|i| cheese_types.include?(i)}
 end
