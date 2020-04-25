0ba0bbc66be931546fda1ec2819abcd6652fa01f
Joey <joey.alzamora@gmail.com>
2020-04-25 04:07:00 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves
+                          # code an argument here
+                           # Your code here
+  array.each_with_index
+  
 end
 
 def summon_captain_planet# code an argument here
@@ -10,7 +13,16 @@ def long_planeteer_calls# code an argument here
   # Your code here
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+                              #code an argument here
+                             # the array below is here to help
+def find_the_cheese(snacks)
+  i = 0
   cheese_types = ["cheddar", "gouda", "camembert"]
+  while i < cheese_types.length
+    return cheese_types[i] 
+    if snacks.include?(cheese_types[i])
+    i += 1
+  end
 end
+  
+
